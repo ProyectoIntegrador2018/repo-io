49297c0c19476dcd366295fd0fdd15ed60d5058a
Oscar Michel Herrera <A00817658@itesm.mx>
2019-03-31 20:51:27 -0600

    Added routes flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :authors
   resources :commits
-  resources :organizations
-  resources :repositories
   get "/auth/:provider/callback", to: "sessions#create"
   get 'auth/failure', to: redirect('/')
   delete 'signout', to: 'sessions#destroy', as: 'signout'
   root to: 'sessions#new'
+  resources :organizations
+  resources :repositories
 end
